b431ac29f5697182714ed2508e96542dd0b29741
joriso <joriso@PingPong.(none)>
2014-06-21 13:44:49 +0200

    "templates "

---
+++ sites/all/modules/mod_muziekladder/theme/agenda_header.tpl.php
@@ -0,0 +1,26 @@
+		<div class="row controls">		
+			<div class="span6">
+				<nav class="prevnextlinks">
+					<div class="prevnextlinks btn-group">
+						<button class="btn prevday"><a href="<?php echo $prevlink ?>"><i class="icon-backward"></i> Vorige dag</a></button>
+						<button class="btn today"><a href="/muziek/"><i class="icon-home"></i> Vandaag</a></button>						
+						<button class="btn nextday"><a href="<?php echo $nextlink ?>">Volgende dag <i class="icon-forward"></i></a></button>
+					</div>	
+				</nav>
+			</div>		
+		
+			<div class="span3 dateselectContainer dayselectContainer">
+				<div class="form-inline">
+					<label>dag:</label>
+					<select class="dayselect"></select>
+				</div>
+			</div>
+			<div class="span3 dateselectContainer monthselectContainer">
+				<div class="form-inline">
+					<label>maand:</label>
+					<select class="monthselect"></select>
+				</div>
+			</div>	
+	
+		</div>
+
