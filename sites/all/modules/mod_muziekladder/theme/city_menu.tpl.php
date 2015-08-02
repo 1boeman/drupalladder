afe783ec99654501919b5a559af229b007db67fa
Joris Osterhaus <joris@hardcode.nl>
2015-08-02 15:02:56 +0200

    lay-out edit

--- sites/all/modules/mod_muziekladder/theme/city_menu.tpl.php
+++ sites/all/modules/mod_muziekladder/theme/city_menu.tpl.php
@@ -9,15 +9,12 @@ if (isset($simple_list)): ?>
   </nav> 
 <?php  
 else:
-
 ?>
 <nav>
 <ul class="front-city-menu clearfix">
 <?php foreach ($cities as $city): ?>
-
   <li class="city city-<?php echo $city['Id']?>" data-id="<?php echo $city['Id']?>"> 
     <a href="<?php echo Muziek_util::city_link($city); ?>">
-      <img alt="<?php echo htmlspecialchars($city['Name']); ?>" src="/<?php echo drupal_get_path('theme',$GLOBALS['theme']).'/city_images/' . $city['Id']?>.jpg" />
       <span class="city-name"><?php echo $city['Name']?></span>
     </a>
 
