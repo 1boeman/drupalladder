8ab31a673ab673647c9473a0f1d6dc97f7488286
Joris Osterhaus <joris@hardcode.nl>
2015-08-29 13:52:50 +0200

    Login mandatory for recomendations

--- sites/all/modules/mod_muziekladder/app/forms.php
+++ sites/all/modules/mod_muziekladder/app/forms.php
@@ -1,206 +1,220 @@
 <?php
 function mod_muziekladder_mailtipform($form, &$form_state) {
-
-   $locaties_db = Muziek_db::open_locaties_db();
-   $cities = Muziek_db::get_cities(1);
-   $city_options = array(t('* City not in list?  Click here... *'));    
-   $venue_options = array(); 
-
-   while ($row = $cities->fetchArray()) {
-      $city_options[$row['Id']] = $row['Name'];
-   }
-   $city_options['00']= '** City not in list? Click here... *';    
-
-  $selected_value = $form_state['input']['city_select'];
-  if (strlen($selected_value) && (int) $selected_value){
-    $venues = Muziek_db::get_city_venues($selected_value); 
-    $venue_options = array('0' =>' ** '.t('Venue not in list? Click here...').' * ');
-    
-    while ( $row = $venues->fetchArray() ){
-      $venue_options[$row['Id']] = html_entity_decode($row['Title']);
-    }                                             
-  }
-
-  $visible_c_f_i = array(
-   'visible'=> array(
-    ':input[name="soort"]' => array(
-          array('value' =>'concert'),
-          array('value' =>'festival'),
-          array('value' =>'iets_anders'),
-    )        
-  ));
-         
-   $form['#prefix'] = '<div class="eventfull">' ;
-   $form['#suffix'] = '</div>' ;
-
-   $form['soort'] = array(
-     '#type' => 'select',
-     '#title' => t('Type of recommendation'),
-     '#options' => array(
-        'concert' => t('Concert or performance'),
-        'festival'=> t('Festival or party'),
-        'iets_anders'=>t('Something completely different')
-     ),
-     '#description' => t('What do you want to recommend to the Muziekladder Calendar ...'),
-     '#required'=> true,
-    );
-
-    $form['item-instucties'] = array(
-        '#type' => 'item',
-        '#attributes' => array('class'=>array('item-instructies')),
-        '#markup' =>  '<p>'.t('Fields marked with * are mandatory').'...</p>',
-        '#states' =>$visible_c_f_i
-
-    ); 
-
-    /* main fieldsets */
-    $form['locatie'] = array(
-        '#type' => 'fieldset',
-        '#attributes' => array('class'=>array('locatie')),
-        '#title' => t('General information'),
-        '#states' =>$visible_c_f_i
-
-    ); 
-    
-    $form['venue'] = array(
-        '#type' => 'fieldset',
-        '#attributes' => array('class'=>array('venue')),
-        '#title' => t('Location'),
-        '#prefix'=>'<div id="venue_fieldset">',
-        '#suffix' => '</div>',
-        '#states' =>$visible_c_f_i
-
-    ); 
-    
-    $form['details'] = array(
-        '#type' => 'fieldset',
-        '#attributes' => array('class'=>array('details')),
-        '#title' => t('Details'),
-        '#states' =>$visible_c_f_i
+  global $user; 
   
-    ); 
+  // only show form if logged in  
+  if ($user->uid) {  
+    $locaties_db = Muziek_db::open_locaties_db();
+    $cities = Muziek_db::get_cities(1);
+    $city_options = array(t('* City not in list?  Click here... *'));    
+    $venue_options = array(); 
+
+    while ($row = $cities->fetchArray()) {
+      $city_options[$row['Id']] = $row['Name'];
+    }
+    $city_options['00']= '** City not in list? Click here... *';    
+
+    $selected_value = $form_state['input']['city_select'];
+    if (strlen($selected_value) && (int) $selected_value){
+      $venues = Muziek_db::get_city_venues($selected_value); 
+      $venue_options = array('0' =>' ** '.t('Venue not in list? Click here...').' * ');
       
-    $form['details']['link'] = array(
-       '#type' => 'textfield',
-       '#title' => t('Link to event or venue'),
-       '#required' => true, 
-       '#attributes' =>array('placeholder' => 'http:// ...... '),
-       '#description' => t('Without a working link we won\'t be able to process this event.'),
-    );
-    
-    $form['details']['title'] = array(
-       '#type' => 'textfield',
-       '#title' => t('Title of event / name of artist(s)'),
-       '#states' =>array(
-          'required' => array (
-            ':input[name="soort"]' => array(
-                array('value' =>'concert'),
-                array('value' =>'festival'),
-            )        
-          )
-       ), 
-
-       '#attributes' =>array('placeholder' => t('Name')),
-    );
-
-    $form['venue']['city_select'] = array(
-       '#type' => 'select',
-       '#title' => t('City'),
-       '#options' => $city_options, 
-       '#attributes' =>array(),
-       '#required' => true, 
-       '#ajax' => array(
-          'callback' => 'ajax_mailtipform_cityselect_callback',
-          'wrapper' => 'venue_fieldset',
-          'method' => 'replace',
-          'effect' => 'fade',
-        ),
-    );
-
-    $form['venue']['city'] = array(
-       '#type' => 'textfield',
-       '#title' => t('Please specify the name of the city, municipality or village hosting the event or performance'),
-       '#states' => array(
-          'visible'=> array(
-            ':input[name="city_select"]' => array(array('value' =>'0'),array('value' =>'00'))
-          ),
-          'required' => array(
-            ':input[name="city_select"]' => array(array('value' =>'0'),array('value' =>'00'))
-          )
-       ),
-       '#attributes' =>array('placeholder' => 
-       t('Name or zip of city,village or area')), 
-        '#required' => false,    
-    );
-
-    if (count ($venue_options) > 1){
-      $form['venue']['venue_select'] = array(
-        '#type'=> 'select',
-        '#title' => t('Location / venue'),
-        '#options' => $venue_options,
-        '#required' => true,
-        '#ajax' => array(
-          'callback' => 'ajax_mailtipform_cityselect_callback',
-          'wrapper' => 'venue_fieldset',
-          'method' => 'replace',
-          'effect' => 'none',
-        ),
-      );
-      
-      $selected_venue = $form_state['input']['venue_select'];
+      while ( $row = $venues->fetchArray() ){
+        $venue_options[$row['Id']] = html_entity_decode($row['Title']);
+      }                                             
     }
 
-    if ( $selected_value == '0' || $selected_value =='00' ||  
-        (isset($selected_venue) && strlen($selected_venue) && $selected_venue == '0')){
-        $form['venue']['venue_freetext'] = array(
-          '#type' => 'textarea',
-          '#title' => t(
-          'Please specify the name and address of the venue, club, area, terrain (or what not) that will host this event.'),
-          '#attributes' => array('placeholder' => 
-          t('Name / address')),
-          
-          '#required' => true );
-    } 
+    $visible_c_f_i = array(
+     'visible'=> array(
+      ':input[name="soort"]' => array(
+            array('value' =>'concert'),
+            array('value' =>'festival'),
+            array('value' =>'iets_anders'),
+      )        
+    ));
+           
+    $form['#prefix'] = '<div class="eventfull">' ;
+    $form['#suffix'] = '</div>' ;
 
+    $form['soort'] = array(
+       '#type' => 'select',
+       '#title' => t('Type of recommendation'),
+       '#options' => array(
+          'concert' => t('Concert or performance'),
+          'festival'=> t('Festival or party'),
+          'iets_anders'=>t('Something completely different')
+       ),
+       '#description' => t('What do you want to recommend to the Muziekladder Calendar ...'),
+       '#required'=> true,
+      );
 
-    $date = format_date(REQUEST_TIME, 'custom', 'd-m-Y');
-    $format = 'd-m-Y';
+      $form['item-instucties'] = array(
+          '#type' => 'item',
+          '#attributes' => array('class'=>array('item-instructies')),
+          '#markup' =>  '<p>'.t('Fields marked with * are mandatory').'...</p>',
+          '#states' =>$visible_c_f_i
 
-    $form['locatie']['date'] = array(
-     '#type' => 'date_select', // types 'date_text' and 'datei_timezone' are also supported. See .inc file.
-     '#title' => t('Date on which the event will take place'),
-     '#default_value' => $date,
-     '#date_format' => $format,
-     '#required' => true
-    );
+      ); 
 
-    $form['details']['description'] = array( 
-     '#type' => 'textarea',
-     '#title' => t('Remarks and/or extra information'),
-     '#attributes' =>array('placeholder' => t('(Support) acts, time, entrance fee, etc.. ')), 
-    );
+      /* main fieldsets */
+      $form['locatie'] = array(
+          '#type' => 'fieldset',
+          '#attributes' => array('class'=>array('locatie')),
+          '#title' => t('General information'),
+          '#states' =>$visible_c_f_i
+
+      ); 
+      
+      $form['venue'] = array(
+          '#type' => 'fieldset',
+          '#attributes' => array('class'=>array('venue')),
+          '#title' => t('Location'),
+          '#prefix'=>'<div id="venue_fieldset">',
+          '#suffix' => '</div>',
+          '#states' =>$visible_c_f_i
+
+      ); 
+      
+      $form['details'] = array(
+          '#type' => 'fieldset',
+          '#attributes' => array('class'=>array('details')),
+          '#title' => t('Details'),
+          '#states' =>$visible_c_f_i
     
-    $form['details']['email'] = array(
-     '#type' => 'textfield',
-     '#title' => t('Your email-address for any other questions that might arise (optional / will of course not be shared in any way on or via this site)'),
-     '#attributes' =>array('placeholder' => t('Email address'))
-    );
-
-    $form['submit'] = array(
-        '#type' => 'submit',
-        '#value' => t('Submit'),
-        '#states' =>$visible_c_f_i
-    );
-
-    $form['item'] = array(
-        '#type' => 'item',
-        '#attributes' => array('class'=>array('item-iets-anders')),
-        '#markup' => '<p>'. t('Your recommendations will be placed on this page, and after a human check also in the Muziekladder Calendar').'</p>'.
+      ); 
         
-              '<p>'.t('
-                For general remarks you may also mail (info at muziekladder.nl) or use twitter ').': <a target="_blank" href="https://twitter.com/muziekladder">@Muziekladder</a></p>'
-    ); 
+      $form['details']['link'] = array(
+         '#type' => 'textfield',
+         '#title' => t('Link to event or venue'),
+         '#required' => true, 
+         '#attributes' =>array('placeholder' => 'http:// ...... '),
+         '#description' => t('Without a working link we won\'t be able to process this event.'),
+      );
+      
+      $form['details']['title'] = array(
+         '#type' => 'textfield',
+         '#title' => t('Title of event / name of artist(s)'),
+         '#states' =>array(
+            'required' => array (
+              ':input[name="soort"]' => array(
+                  array('value' =>'concert'),
+                  array('value' =>'festival'),
+              )        
+            )
+         ), 
 
+         '#attributes' =>array('placeholder' => t('Name')),
+      );
+
+      $form['venue']['city_select'] = array(
+         '#type' => 'select',
+         '#title' => t('City'),
+         '#options' => $city_options, 
+         '#attributes' =>array(),
+         '#required' => true, 
+         '#ajax' => array(
+            'callback' => 'ajax_mailtipform_cityselect_callback',
+            'wrapper' => 'venue_fieldset',
+            'method' => 'replace',
+            'effect' => 'fade',
+          ),
+      );
+
+      $form['venue']['city'] = array(
+         '#type' => 'textfield',
+         '#title' => t('Please specify the name of the city, municipality or village hosting the event or performance'),
+         '#states' => array(
+            'visible'=> array(
+              ':input[name="city_select"]' => array(array('value' =>'0'),array('value' =>'00'))
+            ),
+            'required' => array(
+              ':input[name="city_select"]' => array(array('value' =>'0'),array('value' =>'00'))
+            )
+         ),
+         '#attributes' =>array('placeholder' => 
+         t('Name or zip of city,village or area')), 
+          '#required' => false,    
+      );
+
+      if (count ($venue_options) > 1){
+        $form['venue']['venue_select'] = array(
+          '#type'=> 'select',
+          '#title' => t('Location / venue'),
+          '#options' => $venue_options,
+          '#required' => true,
+          '#ajax' => array(
+            'callback' => 'ajax_mailtipform_cityselect_callback',
+            'wrapper' => 'venue_fieldset',
+            'method' => 'replace',
+            'effect' => 'none',
+          ),
+        );
+        
+        $selected_venue = $form_state['input']['venue_select'];
+      }
+
+      if ( $selected_value == '0' || $selected_value =='00' ||  
+          (isset($selected_venue) && strlen($selected_venue) && $selected_venue == '0')){
+          $form['venue']['venue_freetext'] = array(
+            '#type' => 'textarea',
+            '#title' => t(
+            'Please specify the name and address of the venue, club, area, terrain (or what not) that will host this event.'),
+            '#attributes' => array('placeholder' => 
+            t('Name / address')),
+            
+            '#required' => true );
+      } 
+
+
+      $date = format_date(REQUEST_TIME, 'custom', 'd-m-Y');
+      $format = 'd-m-Y';
+
+      $form['locatie']['date'] = array(
+       '#type' => 'date_select', // types 'date_text' and 'datei_timezone' are also supported. See .inc file.
+       '#title' => t('Date on which the event will take place'),
+       '#default_value' => $date,
+       '#date_format' => $format,
+       '#required' => true
+      );
+
+      $form['details']['description'] = array( 
+       '#type' => 'textarea',
+       '#title' => t('Remarks and/or extra information'),
+       '#attributes' =>array('placeholder' => t('(Support) acts, time, entrance fee, etc.. ')), 
+      );
+      
+      $form['details']['email'] = array(
+       '#type' => 'textfield',
+       '#title' => t('Your email-address for any other questions that might arise (optional / will of course not be shared in any way on or via this site)'),
+       '#attributes' =>array('placeholder' => t('Email address'))
+      );
+
+      $form['submit'] = array(
+          '#type' => 'submit',
+          '#value' => t('Submit'),
+          '#states' =>$visible_c_f_i
+      );
+
+    } else {
+   
+   
+       $form['item2'] = array(
+          '#type' => 'item',
+          '#attributes' => array('class'=>array('item-iets-anders')),
+          '#markup' => '<p><a href="'.Muziek_util::lang_url().'user?destination=muziekformulier">'.t('Please log in or register to recommend your events to Muziekladder').'</a></p>'
+          
+      ); 
+    } 
+  
+    $form['item'] = array(
+          '#type' => 'item',
+          '#markup' => '<p>'. t('Your recommendations will be placed on this page, and after a human check also in the Muziekladder Calendar').'</p>'.
+          
+                '<p>'.t('
+                  For general remarks you may also mail (info at muziekladder.nl) or use twitter ').': <a target="_blank" href="https://twitter.com/muziekladder">@Muziekladder</a></p>'
+      ); 
+
+  
     return $form;
 }
 
