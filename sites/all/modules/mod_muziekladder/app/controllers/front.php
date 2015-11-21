8481dd34714b71f2ab2a383e8be12fc5cfd6e433
Joris Osterhaus <joris@hardcode.nl>
2015-11-22 00:30:26 +0100

    Tips archief toegevoegd

--- sites/all/modules/mod_muziekladder/app/controllers/front.php
+++ sites/all/modules/mod_muziekladder/app/controllers/front.php
@@ -1,31 +1,30 @@
 <?php
-require 'muziek.php'; 
+require 'muziek.php';
 class Front extends Controller {
 	public function index () {
-      global $language; 
-      	
+      global $language;
+
       $this->set_head_title(t('Muziekladder: calendar for live music, concerts and parties in &amp; around the Netherlands '));
       if ($language->prefix == 'en'){
         $this->set_meta_desc('Calendar, locations and tips for live music, concerts, parties and shows in the Netherlands and surrounding areas.');
         $this->set_title('Music, concerts, parties and festivals');
-        $ga_naar = 'Add tips';
+        $ga_naar = 'Tips archive';
 
       }else{
         $this->set_meta_desc('Agenda, locaties en tips voor live muziek, concerten, feestjes en shows  in Nederland en omstreken.');
         $this->set_title('Muziek, concerten, feesten en festivals');
-        $ga_naar = 'Tips toevoegen';
+        $ga_naar = 'Tips archief';
 
       }
 
       $s = Singleton::get_instance();
       $m = new Muziek();
-      $s->agenda = ' <a class="naar-agenda-link btn btn-inverse" href="/'.$language->prefix.'/muziekformulier">'.$ga_naar.' &raquo;</a>';
-      
+      $s->agenda = ' <a class="naar-agenda-link btn btn-inverse" href="/'.$language->prefix.'/archief">'.$ga_naar.' &raquo;</a>';
+
       $cities = Muziek_db::get_cities_by_ids(array(1,8,5,1412801590,1413406572,4,7,15,6,17,100,1439757759));
       shuffle($cities);
-      $s->city_menu = theme('city_menu',array('cities' => $cities)); 
+      $s->city_menu = theme('city_menu',array('cities' => $cities));
 
 
    	}
 }
-
--- sites/all/modules/mod_muziekladder/theme/tip_node.tpl.php
+++ sites/all/modules/mod_muziekladder/theme/tip_node.tpl.php
@@ -5,15 +5,15 @@
     <a class="user_link_in_node" data-uid="<?php echo $tip['uid'] ?>"  href="<?php echo Muziek_util::lang_url().'user/'.$tip['uid']  ?>"><?php echo $tip['user_name'] ?></a>
   </div>
   <?php endif; ?>
-  <ul class="event_data_in_node">
+  <ul class="event_data_in_node event-data-in-node">
     <li class="event_link_in_node"><a href="<?php echo $tip['link'] ?>" target="_blank"><?php echo $tip['link'] ?></a></li>
-    <li class="venue_in_node"><?php 
+    <li class="venue_in_node"><?php
         if ( $tip['venue_select']){
           if ($tip['db_venue']) {
-          ?><a href="<?php echo $tip['locatie_link']?>"><?php echo $tip['db_venue']['Title'] ?></a> <?php 
-          } else { 
+          ?><a href="<?php echo $tip['locatie_link']?>"><?php echo $tip['db_venue']['Title'] ?></a> <?php
+          } else {
             echo $tip['venue_select'];
-          }   
+          }
         } else {?>
           <div class="freetext_in_node">
           <?php echo nl2br(filter_xss($tip['venue_freetext']));?>
@@ -22,14 +22,14 @@
         }
       ?>
     </li>
-    <li class="city_in_node"><span><?php 
+    <li class="city_in_node"><span><?php
       if ($tip['city_select'] && isset($tip['db_city']) && $tip['db_city']){
         echo $tip['db_city']['Name'] ;
       } elseif (isset($tip['city'])) {
          echo filter_xss($tip['city']);
       } else{
-        echo t('unknown'); 
-      } 
+        echo t('unknown');
+      }
     ?></span>
     </li>
     <li class="dates_in_node">
@@ -41,15 +41,15 @@
       <?php endforeach ?>
       </ul>
     </li>
- 
-  <?php if (!$summary) : ?> 
+
+  <?php if (!$summary) : ?>
     <li class="soort_in_node"><?php echo $tip['soort'] ?></li>
   <?php endif; ?>
 
   </ul>
-  <?php if (!$summary) : ?> 
+  <?php if (!$summary) : ?>
   <p class="description_in_node">
     <?php echo nl2br(filter_xss($tip['description'])); ?>
   </p>
   <?php endif; ?>
-</div>  
+</div>
