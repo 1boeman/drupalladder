d7cd60334dea367f784479171208f1b8e2e07e9f
Joris Osterhaus <joris@hardcode.nl>
2015-12-07 23:11:56 +0100

    getting rid of #domid notice in views

--- sites/all/modules/mod_muziekladder/app/controllers/muziekformulier.php
+++ sites/all/modules/mod_muziekladder/app/controllers/muziekformulier.php
@@ -32,6 +32,10 @@ class Muziekformulier extends Controller {
 
     $view = views_get_view('recent_tips');
     $view->set_display('page');
+    $args = $view->args;
+    $args[0] = $node->nid;
+    $view->set_arguments($args);
+    $view->pre_execute();
     $view->execute();
 
     $response = $view->render();
