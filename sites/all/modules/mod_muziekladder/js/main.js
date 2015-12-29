1a34e5d424fa510967ca63f9af1c1b3b34017241
Joris Osterhaus <joris@hardcode.nl>
2015-12-29 23:37:29 +0100

    lazyload agenda images

--- sites/all/modules/mod_muziekladder/js/main.js
+++ sites/all/modules/mod_muziekladder/js/main.js
@@ -295,8 +295,8 @@ var hC = Drupal.settings.muziekladder;
   }
 
   function loadAgendaImages(){
-    // temp disable:
-    var i = 0;
+
+
     $('.city_gig').each(function(){
       var $gig = $(this);
       var src;
@@ -306,18 +306,104 @@ var hC = Drupal.settings.muziekladder;
 
       if (img_code.match(/^data/)){
         src = img_code;
+        img_html = '<img src="'+src+'" />'
       }else{
         src = '/muziekdata/img/?s=1&p='+img_code;
+        img_html = '<img src="'+Drupal.settings.muziekladder.pathToTheme+'/img/blank.gif" class="lazy" data-src="'+src+'" />';
       }
 
-      i++;
-      if (i>24) return
       var img = new Image;
-      img.onload = function(){
-        $gig.find('.first-cell').prepend('<div class="image-cell"><img src="'+src+'" /></div>')
-      }
-      img.src = src;
+      $gig.find('.first-cell').prepend('<div class="image-cell">'+img_html+'</div>')
     });
+
+
+
+
+
+
+      /* lazyload.js (c) Lorenzo Giuliani
+     * MIT License (http://www.opensource.org/licenses/mit-license.html)
+     *
+     * expects a list of:
+     * `<img src="blank.gif" data-src="my_image.png" width="600" height="400" class="lazy">`
+     */
+
+    !function(){
+      var $q = function(q, res){
+            if (document.querySelectorAll) {
+              res = document.querySelectorAll(q);
+            } else {
+              var d=document
+                , a=d.styleSheets[0] || d.createStyleSheet();
+              a.addRule(q,'f:b');
+              for(var l=d.all,b=0,c=[],f=l.length;b<f;b++)
+                l[b].currentStyle.f && c.push(l[b]);
+
+              a.removeRule(0);
+              res = c;
+            }
+            return res;
+          }
+        , addEventListener = function(evt, fn){
+            window.addEventListener
+              ? this.addEventListener(evt, fn, false)
+              : (window.attachEvent)
+                ? this.attachEvent('on' + evt, fn)
+                : this['on' + evt] = fn;
+          }
+        , _has = function(obj, key) {
+            return Object.prototype.hasOwnProperty.call(obj, key);
+          }
+        ;
+
+      function loadImage (el, fn) {
+        var img = new Image()
+          , src = el.getAttribute('data-src');
+        img.onload = function() {
+          if (!! el.parent)
+            el.parent.replaceChild(img, el)
+          else
+            el.src = src;
+
+          fn? fn() : null;
+        }
+        img.onerror = function(){
+          el.parentNode.style.display = 'none';
+        }
+        img.src = src;
+      }
+
+      function elementInViewport(el) {
+        var rect = el.getBoundingClientRect()
+
+        return (
+           rect.top    >= 0
+        && rect.left   >= 0
+        && rect.top <= (window.innerHeight || document.documentElement.clientHeight)
+        )
+      }
+
+        var images = new Array()
+          , query = $q('img.lazy')
+          , processScroll = function(){
+              for (var i = 0; i < images.length; i++) {
+                if (elementInViewport(images[i])) {
+                  loadImage(images[i], function () {
+                    images.splice(i, i);
+                  });
+                }
+              };
+            }
+          ;
+        // Array.prototype.slice.call is not callable under our lovely IE8
+        for (var i = 0; i < query.length; i++) {
+          images.push(query[i]);
+        };
+
+        processScroll();
+        addEventListener('scroll',processScroll);
+
+    }();
   };
   hC.loadAgendaImages = loadAgendaImages;
 
